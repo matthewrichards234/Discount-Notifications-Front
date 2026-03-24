7ee11ac83db5ad67a8e0f34352955dcb16adeb76
Matthew Richards <matthewrichards234@gmail.com>
2026-03-24 12:40:24 -0400

    fix: delete unused imports

--- src/APITester.tsx
+++ src/APITester.tsx
@@ -1,8 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
 import { useRef, type FormEvent } from "react";
 
 export function APITester() {
@@ -26,39 +21,5 @@ export function APITester() {
     }
   };
 
-  return (
-    <div className="flex flex-col gap-6">
-      <form onSubmit={testEndpoint} className="flex items-center gap-2">
-        <Label htmlFor="method" className="sr-only">
-          Method
-        </Label>
-        <Select name="method" defaultValue="GET">
-          <SelectTrigger className="w-[100px]" id="method">
-            <SelectValue placeholder="Method" />
-          </SelectTrigger>
-          <SelectContent align="start">
-            <SelectItem value="GET">GET</SelectItem>
-            <SelectItem value="PUT">PUT</SelectItem>
-          </SelectContent>
-        </Select>
-        <Label htmlFor="endpoint" className="sr-only">
-          Endpoint
-        </Label>
-        <Input id="endpoint" type="text" name="endpoint" defaultValue="/api/hello" placeholder="/api/hello" />
-        <Button type="submit" variant="secondary">
-          Send
-        </Button>
-      </form>
-      <Label htmlFor="response" className="sr-only">
-        Response
-      </Label>
-      <Textarea
-        ref={responseInputRef}
-        id="response"
-        readOnly
-        placeholder="Response will appear here..."
-        className="min-h-[140px] font-mono resize-y"
-      />
-    </div>
-  );
+  return <div className="flex flex-col gap-6"></div>;
 }
