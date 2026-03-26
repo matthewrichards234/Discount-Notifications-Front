24dd3697d2a93409f5cc3c9ebeff81e7c403ec74
Matthew Richards <matthewrichards234@gmail.com>
2026-03-26 19:58:51 -0400

    feat: Add boilerplates for login, signup, and profile components. Add routing to all three.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
       <h1 className="text-blue-500">Hello</h1>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
     </>
   );
 }
