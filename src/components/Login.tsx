10e63131b2ada9bdfc9183f2a5fa42eac24ab6b9
Matthew Richards <matthewrichards234@gmail.com>
2026-03-27 13:40:17 -0400

    style: add styles to error messages on login form.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -18,6 +18,7 @@ const Login = () => {
   const onSubmit: SubmitHandler<ILoginInput> = (data) => console.log(data);
 
   const labelClassName: string = "text-2xl underline underline-offset-2";
+  const errorMessageClass: string = "text-red-500 text-[12px]";
 
   return (
     <>
@@ -30,7 +31,9 @@ const Login = () => {
           className="border-3"
         />
         {errors.firstName?.type === "required" && (
-          <p role="alert">First name is required</p>
+          <p role="alert" className={errorMessageClass}>
+            First name is required
+          </p>
         )}
         <label className={labelClassName}>Last Name</label>
         <input
@@ -40,7 +43,9 @@ const Login = () => {
           className="border-3"
         />
         {errors.lastName?.type === "required" && (
-          <p role="alert">Last name is required</p>
+          <p role="alert" className={errorMessageClass}>
+            Last name is required
+          </p>
         )}
         <label className={labelClassName}>Email</label>
         <input
@@ -50,7 +55,9 @@ const Login = () => {
           className="border-3"
         />
         {errors.email?.type === "required" && (
-          <p role="alert">Email is required</p>
+          <p role="alert" className={errorMessageClass}>
+            Email is required
+          </p>
         )}
         <label className={labelClassName}>Password</label>
         <input
@@ -59,7 +66,11 @@ const Login = () => {
           type="text"
           className="border-3"
         />
-        {errors.password && <p role="alert">{errors.password.message}</p>}
+        {errors.password && (
+          <p role="alert" className={errorMessageClass}>
+            {errors.password.message}
+          </p>
+        )}
         <label className={labelClassName}>Confirm Password</label>
         <input
           {...register("confirmPassword", {
@@ -69,7 +80,9 @@ const Login = () => {
           className="border-3"
         />
         {errors.confirmPassword && (
-          <p role="alert">{errors.confirmPassword.message}</p>
+          <p role="alert" className={errorMessageClass}>
+            {errors.confirmPassword.message}
+          </p>
         )}
         <input type="submit" />
       </form>
