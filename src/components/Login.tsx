110729a74fe7a7d30e299465c0c72f70f699faae
Matthew Richards <matthewrichards234@gmail.com>
2026-03-27 10:29:07 -0400

    Feat: Add useForm, first/last name fields / submit button to login form.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,31 @@
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+
+interface ILoginInput {
+  firstName: string;
+  lastName: string;
+}
+
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit: SubmitHandler<ILoginInput> = (data) => console.log(data);
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+        <label>First Name</label>
+        <input {...register("firstName")} type="text" className="border-3" />
+        <label>Last Name</label>
+        <input {...register("lastName")} type="text" className="border-3" />
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
 
 export default Login;
+
+/* 
+Login Form:
+- Use react form hook
+- Create login with google button
+*/
