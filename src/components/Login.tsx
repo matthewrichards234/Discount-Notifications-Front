c6fd2997e6395ed182dc534f063916bd3b5b87ea
Matthew Richards <matthewrichards234@gmail.com>
2026-03-28 12:17:07 -0400

    style: add background gray wrap around and shadow to improve aesthetics.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -21,78 +21,80 @@ const Login = () => {
     "flex flex-col justify-items-center items-center";
   const labelClassName: string = "text-2xl underline underline-offset-2";
   const errorMessageClassName: string = "text-red-500 text-[12px]";
-  const borderClassName: string = "border-2 w-50 rounded-lg";
+  const borderClassName: string = "border-2 w-50 rounded-lg m-2";
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} className={formClassName}>
-        <label className={labelClassName}>First Name</label>
-        <input
-          {...register("firstName", { required: true })}
-          aria-invalid={errors.firstName ? "true" : "false"}
-          type="text"
-          className={borderClassName}
-        />
-        {errors.firstName?.type === "required" && (
-          <p role="alert" className={errorMessageClassName}>
-            First name is required
-          </p>
-        )}
-        <label className={labelClassName}>Last Name</label>
-        <input
-          {...register("lastName", { required: true })}
-          aria-invalid={errors.lastName ? "true" : "false"}
-          type="text"
-          className={borderClassName}
-        />
-        {errors.lastName?.type === "required" && (
-          <p role="alert" className={errorMessageClassName}>
-            Last name is required
-          </p>
-        )}
-        <label className={labelClassName}>Email</label>
-        <input
-          {...register("email", { required: true })}
-          aria-invalid={errors.email ? "true" : "false"}
-          type="text"
-          className={borderClassName}
-        />
-        {errors.email?.type === "required" && (
-          <p role="alert" className={errorMessageClassName}>
-            Email is required
-          </p>
-        )}
-        <label className={labelClassName}>Password</label>
-        <input
-          {...register("password", { required: "Email is required" })}
-          aria-invalid={errors.password ? "true" : "false"}
-          type="text"
-          className={borderClassName}
-        />
-        {errors.password && (
-          <p role="alert" className={errorMessageClassName}>
-            {errors.password.message}
-          </p>
-        )}
-        <label className={labelClassName}>Confirm Password</label>
-        <input
-          {...register("confirmPassword", {
-            required: "Password confirmation is required",
-          })}
-          type="text"
-          className={borderClassName}
-        />
-        {errors.confirmPassword && (
-          <p role="alert" className={errorMessageClassName}>
-            {errors.confirmPassword.message}
-          </p>
-        )}
-        <input
-          type="submit"
-          className="bg-sky-500 hover:bg-sky-700 text-black font-semibold py-2 px-4 rounded"
-        />
-      </form>
-    </>
+    <div className="bg-gray-200 p-6 flex justify-center items-center min-h-screen">
+      <div className="border-2 rounded p-6 bg-white w-200 shadow-lg">
+        <form onSubmit={handleSubmit(onSubmit)} className={formClassName}>
+          <label className={labelClassName}>First Name</label>
+          <input
+            {...register("firstName", { required: true })}
+            aria-invalid={errors.firstName ? "true" : "false"}
+            type="text"
+            className={borderClassName}
+          />
+          {errors.firstName?.type === "required" && (
+            <p role="alert" className={errorMessageClassName}>
+              First name is required
+            </p>
+          )}
+          <label className={labelClassName}>Last Name</label>
+          <input
+            {...register("lastName", { required: true })}
+            aria-invalid={errors.lastName ? "true" : "false"}
+            type="text"
+            className={borderClassName}
+          />
+          {errors.lastName?.type === "required" && (
+            <p role="alert" className={errorMessageClassName}>
+              Last name is required
+            </p>
+          )}
+          <label className={labelClassName}>Email</label>
+          <input
+            {...register("email", { required: true })}
+            aria-invalid={errors.email ? "true" : "false"}
+            type="text"
+            className={borderClassName}
+          />
+          {errors.email?.type === "required" && (
+            <p role="alert" className={errorMessageClassName}>
+              Email is required
+            </p>
+          )}
+          <label className={labelClassName}>Password</label>
+          <input
+            {...register("password", { required: "Email is required" })}
+            aria-invalid={errors.password ? "true" : "false"}
+            type="text"
+            className={borderClassName}
+          />
+          {errors.password && (
+            <p role="alert" className={errorMessageClassName}>
+              {errors.password.message}
+            </p>
+          )}
+          <label className={labelClassName}>Confirm Password</label>
+          <input
+            {...register("confirmPassword", {
+              required: "Password confirmation is required",
+            })}
+            type="text"
+            className={borderClassName}
+          />
+          {errors.confirmPassword && (
+            <p role="alert" className={errorMessageClassName}>
+              {errors.confirmPassword.message}
+            </p>
+          )}
+          <input
+            type="submit"
+            className="bg-sky-500 hover:bg-sky-700 text-black font-semibold py-2 px-4 rounded mt-5 w-50 h-12"
+          />
+        </form>
+      </div>
+    </div>
   );
 };
 
