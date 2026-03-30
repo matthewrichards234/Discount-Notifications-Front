686aa6cecd9a5dca0a1e9810b0fe2315be2559ce
Matthew Richards <matthewrichards234@gmail.com>
2026-03-29 20:07:30 -0400

    feat: add dark mode.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,18 +4,27 @@ import Login from "./Login";
 import Signup from "./Signup";
 import Profile from "./Profile";
 import UploadForm from "./UploadForm";
+import { DarkModeContext } from "../context/DarkModeContext";
+import { useContext } from "react";
 
 function App() {
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const handleDarkModeToggle = () => {
+    toggleDarkMode();
+  };
   return (
-    <>
+    <div
+      className={`${darkMode ? "bg-black text-white" : "bg-white text-black"} min-h-screen`}
+    >
+      <button onClick={handleDarkModeToggle}>Light or Dark ahh mode</button>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="upload" element={<UploadForm />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/upload" element={<UploadForm />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./components/App.tsx";
+import { DarkModeProvider } from "./context/DarkModeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
     </BrowserRouter>
   </StrictMode>,
 );
