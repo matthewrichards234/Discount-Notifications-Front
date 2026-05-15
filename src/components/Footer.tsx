958b8681fca50a7d99cb8c8604236cc86cad7373
Matthew Richards <matthewrichards234@gmail.com>
2026-05-15 10:44:30 -0400

    add links to connect in footer as well as padding between sections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,17 @@ import { NavLink } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-100 p-10">
+    <div className="bg-gray-100 p-10 flex flex-row items-center justify-between">
       <NavLink to={"/about-us"}>About Us</NavLink>
+      <a
+        href="https://www.linkedin.com/in/matthew-richards234/"
+        target="_blank"
+      >
+        LinkedIn
+      </a>
+      <a href="https://github.com/matthewrichards234" target="_blank">
+        GitHub
+      </a>
     </div>
   );
 };
