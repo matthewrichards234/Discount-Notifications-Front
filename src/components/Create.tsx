ce4a01b21746c9b2ea7695e221c5bff158c377cb
Matthew Richards <matthewrichards234@gmail.com>
2026-05-15 14:39:59 -0400

    add error message on form

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -19,8 +19,9 @@ const Create = () => {
         className="flex flex-col justify-items-center items-center"
       >
         <p className="text-5xl">UPLOAD</p>
-        <label>Paste URL below...</label>
+        <label htmlFor="website">Paste URL below...</label>
         <input
+          id="website"
           {...register("website", {
             required: "URL is required",
             validate: (value) => {
@@ -35,6 +36,9 @@ const Create = () => {
           type="text"
           className="border-2 rounded-lg m-2 w-120"
         />
+        {errors.website && errors.website.type === "required" && (
+          <span className="text-red-700">Please enter a URL</span>
+        )}
         <input
           type="submit"
           className="bg-sky-500 hover:bg-sky-700 text-black font-semibold py-2 px-4 rounded mt-5 w-50 h-12 disabled:opacity-40 disabled:pointer-events-none"
