2b4078895d0ae1ee12bd96b137ad8fbd2f2e4e59
Matthew Richards <matthewrichards234@gmail.com>
2026-05-23 10:59:08 -0400

    add props to GoogleLogin and install jwtDecoder for user info later.

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,14 +1,31 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router";
 
 const Signin = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       Signin
       <GoogleLogin
         onSuccess={(credentialResponse) => {
           console.log(credentialResponse);
+          console.log(jwtDecode(credentialResponse.credential));
+          navigate("/"); // Navigate user to home after successful login.
+          /* To - Do:
+          1. Change state of "Sign-In" button to "Profile" button upon successful login.
+          2. Add Logout functionality via 'googleLogout' from '@react-oauth.google'.
+          */
         }}
         onError={() => console.log("Login Failed")}
+        auto_select={true} // Automatic sign-ins for users who have signed in in the past.
+        width={400}
+        shape="pill"
+        size="large"
+        cancel_on_tap_outside={true}
+        ux_mode="redirect"
+        type="standard"
       />
       <p>Continue w/ Apple</p>
       <p>or</p>
